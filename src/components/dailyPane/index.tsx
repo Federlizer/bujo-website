f50f8ffa1bca06c13ccd7fc373f8b140cfdcd400
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-06-17 12:30:50 +0200

    Order tasks before displaying them

--- src/components/dailyPane/index.tsx
+++ src/components/dailyPane/index.tsx
@@ -24,9 +24,15 @@ export const DailyPane = (props: DailyPaneProps) => {
     deleteTask,
   } = props;
 
+  const orderedTasks = tasks.sort((a, b) => {
+    if (a.date.isBefore(b.date)) return -1;
+    if (a.date.isAfter(b.date)) return 1;
+    return 0;
+  });
+
   const taskDateMap: { [dateString: string]: Task[] } = {};
 
-  tasks.forEach((task) => {
+  orderedTasks.forEach((task) => {
     const dateString = task.date.format('YYYY-MM-DD');
 
     if (Object.prototype.hasOwnProperty.call(taskDateMap, dateString)) {
